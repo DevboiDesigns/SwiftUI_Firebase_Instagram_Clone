50891b706ef5fd276df1aef33c773e020ffd5f6b
Christopher <visrule@protonmail.com>
2021-04-14 12:26:08 -0500

    Initial Commit

---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Christopher Hicks on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
